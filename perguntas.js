71f122cd2289935f12791307ca4fe07efaf34ac1
larissaabc23 <168578816+larissaabc23@users.noreply.github.com>
2024-11-02 14:27:57 -0300

    adicionando arquivos no git

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esporte',
+    'Qual é o time de vôlei feminino mais vitorioso da Superliga Brasileira??',
+    'O Rexona-Ades (atualmente chamado de Sesc RJ). '
+)
+
+criaCartao(
+    'Esporte',
+    'Qual é a importância do Campeonato Mundial de Vôlei Feminino?',
+    'É a principal competição internacional de seleções, organizada a cada quatro anos, onde as melhores equipes do mundo competem pelo título mundial. '
+)
+
+criaCartao(
+    'Esporte',
+    'Qual jogadora brasileira é considerada uma das melhores atacantes da história do vôlei feminino? ',
+    'Fernanda Garay'
+)
+
+criaCartao(
+    'Esporte',
+    'O que é a Copa do Mundo de Vôlei?',
+    'É um torneio internacional que reúne seleções de todo o mundo e serve como uma das principais competições para a classificação para os Jogos Olímpicos.'
+)
